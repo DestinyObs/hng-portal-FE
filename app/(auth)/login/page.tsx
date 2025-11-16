e077b0db47891145901bc8d2effc41154422b805
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-16 21:02:46 +0100

    feat: add folder structure

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+    )
+}
+
