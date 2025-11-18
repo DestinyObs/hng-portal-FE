a25cb7b4272ec40b9f046da7273bb18d07e62ec6
Truella <turahsuleiman1212@gmail.com>
2025-11-18 20:30:29 +0100

    fix: resolve unwanted scrollbar visibility on wide viewports

--- components/landing-page/hero/hero-image-container.tsx
+++ components/landing-page/hero/hero-image-container.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 export default function HeroImageContainer() {
   return (
-    <div className="w-[85%] mx-auto max-w-[320px] md:max-w-[560px] not-md:mt-[64px]  relative">
+    <div className="w-[85%] mx-auto max-w-[320px] md:max-w-[560px] not-md:mt-16  relative">
       <Image
         src={
           '/assets/images/landing-page/hero-images/desktop/square_bracket.png'
