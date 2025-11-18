a7c640503e9c45709b75314e007468b0a088a00a
cynthia <abahojomacynthia@gmail.com>
2025-11-18 18:06:40 +0100

    fix(design-system): fixed the default page

--- app/(external-pages)/page.tsx
+++ app/(external-pages)/page.tsx
@@ -2,31 +2,13 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { AccordionUI } from '@/components/shared/accordion';
-import Dropdown from '@/components/shared/dropdown';
 
 export default function Page() {
   const router = useRouter();
   
-  // useEffect(() => {
-  //   router.push('/waitlist');
-  // }, [router]);
+  useEffect(() => {
+    router.push('/waitlist');
+  }, [router]);
 
-  // return null;
-  return (
-    <div>
-      <AccordionUI content={[{title: 'sdfgf', content: 'wdfghgfdsf'}]} />
-      <Dropdown title={'sdfgfd'} triggerClassName='w-full' values={[
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'},
-        {name: 'dfvgtvfrdcsf'}]} />
-    </div>
-  )
+  return null;
 }
