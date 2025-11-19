e9af88cb2f9963022100e389199d6eebc2884c4e
Echioda Mathias <mechioda@gmail.com>
2025-11-19 09:43:26 +0100

    fix:importation of whychoose us and howitworks

--- app/(external-pages)/landing-page/page.tsx
+++ app/(external-pages)/landing-page/page.tsx
@@ -3,6 +3,8 @@ import LogoCarousel from '@/components/landing-page/carousel'
 import HeroSection from '@/components/landing-page/hero/hero-section'
 import Features from '@/components/landing-page/features/features';
 import React from 'react'
+import WhyChooseUs from '@/components/landing-page/why-choose-us/WhyChooseUs';
+import HowItWorks from '@/components/landing-page/how-it-works/HowItWorks';
 export default function page() {
   return (
     <section className="min-h-screen bg-white">
@@ -10,6 +12,8 @@ export default function page() {
       <HeroSection />
       <LogoCarousel />
       <Features />
+      <WhyChooseUs/>
+      <HowItWorks/>
     </section>
   );
 }
