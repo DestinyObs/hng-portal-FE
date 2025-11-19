8846f54d71b8638b2a6e6ebc790bef4a4b22680c
cynthia <abahojomacynthia@gmail.com>
2025-11-19 17:01:25 +0100

    feat(landing-page): add faq section and customer review section

--- app/(external-pages)/landing-page/page.tsx
+++ app/(external-pages)/landing-page/page.tsx
@@ -1,10 +1,10 @@
-
 import LogoCarousel from '@/components/landing-page/carousel'
 import HeroSection from '@/components/landing-page/hero/hero-section'
 import Features from '@/components/landing-page/features/features';
-import React from 'react'
 import WhyChooseUs from '@/components/landing-page/why-choose-us/WhyChooseUs';
 import HowItWorks from '@/components/landing-page/how-it-works/HowItWorks';
+import CustomerReview from '@/components/landing-page/customer-review/customer-review';
+import { FAQ } from '@/components/FAQ/faq';
 export default function page() {
   return (
     <section className="min-h-screen bg-white">
@@ -13,6 +13,9 @@ export default function page() {
       <Features />
       <WhyChooseUs/>
       <HowItWorks/>
+      {/* LANDING PAGE CONT */}
+      <CustomerReview />
+      <FAQ />
     </section>
   );
 }
