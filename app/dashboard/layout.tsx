8acbaefebff3d50fc665daf6ac6174c4f03f9f31
CodedLibra <adeola.ogundijo@yahoo.com>
2025-11-23 23:57:18 +0100

    chore

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,25 +1,22 @@
-import Sidebar from '@/components/dashboard/sidebar'
-import { ReactNode } from 'react'
+import Sidebar from '@/components/dashboard/sidebar';
+import { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-screen overflow-hidden flex flex-col bg-white-100">
       {/* HEADER */}
-      <header className="bg-white h-16 flex items-center px-6">
-      </header>
+      <header className="bg-white h-16 flex items-center px-6"></header>
 
       {/* SIDEBAR & MAIN CONTENT */}
-      <div className="flex flex-1 min-h-0 p-6 gap-6">
+      <div className="flex flex-1 min-h-0 p-6 gap-8">
         {/* Sidebar */}
         <Sidebar />
 
         {/* Main content */}
-        <main className="flex-1 h-full overflow-y-scroll">
-          {children}
-        </main>
+        <main className="flex-1 h-full overflow-y-scroll">{children}</main>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
