0ed4ac281e67577481c96e86d835555361390a04
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-11-25 13:42:59 +0100

    fix: fix design inconsistencies with the hero-section of the landing-page

--- components/landing-page/hero/hero-section.tsx
+++ components/landing-page/hero/hero-section.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import HeroText from './hero-text';
 import HeroImageContainer from './hero-image-container';
+import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <section className="flex justify-center items-center">
-      <div className="w-full max-w-[1200] flex flex-col md:flex-row lg:gap-[83px] md:justify-center md:mt-[100px] gap-14 md:px-4">
+    <section className="flex justify-center items-center bg-primary-300 pb-10 relative">
+      <div className="w-full max-w-[1200] flex flex-col md:flex-row lg:gap-[83px] md:justify-center md:mt-[100px] gap-14 md:px-4 ">
         <HeroText />
         <HeroImageContainer />
+
+        <Image
+          src={
+            '/assets/images/landing-page/hero-images/desktop/hero_bg_items.png'
+          }
+          alt="smiling user"
+          width={800}
+          height={700}
+          className="absolute bottom-0 right-0 z-0 opacity-70"
+        />
       </div>
     </section>
   );
--- components/landing-page/hero/hero-text.tsx
+++ components/landing-page/hero/hero-text.tsx
@@ -5,22 +5,29 @@ import Link from 'next/link';
 export default function HeroText() {
   return (
     <div className="not-sm:mt-11 mx-auto max-w-[558px] md:text-left text-center flex flex-col justify-center ">
-      <Badge variant={'outline'} className="border-primary-300 not-md:mx-auto">
+      <Badge
+        variant={'outline'}
+        className="bg-white not-md:mx-auto border-0 p-2"
+      >
         {' '}
         Connecting talent with Opportunity
       </Badge>
-      <h1 className="text-[32px] md:text-[52px] mt-4 md:my-6 font-semibold leading-tight">
+      <h1 className="text-[32px] md:text-[52px] text-white mt-4 md:my-6 font-semibold leading-tight">
         Your Career Platform, Reimagined.
       </h1>
 
-      <p className="not-sm:w-[90%] mx-auto text-[#4A5565] mt-3 md:mt-4 mb-4 md:mb-6">
+      <p className="not-sm:w-[90%] mx-auto text-white mt-3 md:mt-4 mb-4 md:mb-6">
         {
           "Whether you're building your career or building your team, HNG Portal connects you with the right opportunities."
         }
       </p>
       <div className="flex gap-4 justify-center md:justify-start items-center">
         <Link href="/sign-up/talent" passHref>
-          <Button asChild className="px-4 py-5 md:py-6 text-[14px]">
+          <Button
+            asChild
+            variant="outline"
+            className="px-4 py-5 md:py-6 text-[14px] rounded-3xl bg-white border text-primary-blue"
+          >
             <span>Join as Talent</span>
           </Button>
         </Link>
@@ -28,8 +35,7 @@ export default function HeroText() {
         <Link href="/sign-up/company" passHref>
           <Button
             asChild
-            variant="outline"
-            className="px-4 py-5 md:py-6 text-[14px]"
+            className="px-4 py-5 md:py-6 text-[14px] rounded-3xl border border-white"
           >
             <span>Hire a Talent</span>
           </Button>
