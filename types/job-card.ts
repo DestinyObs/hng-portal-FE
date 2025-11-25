b737bde1010e2627aed9b190a278dbe925a2562b
CodedLibra <adeola.ogundijo@yahoo.com>
2025-11-25 03:43:29 +0100

    feta: added types to Job-card component and rendered props value

---
+++ types/job-card.ts
@@ -0,0 +1,15 @@
+export interface JobCardProps {
+  job: Partial<{
+    id: string;
+    title: string;
+    status: string;
+    applicants?: number;
+    job_type: {
+      name: string;
+    };
+    created_at: string | undefined;
+    countries: {
+      name: string;
+    };
+  }>;
+}
