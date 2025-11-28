eca9da8c9dcabc8766d0296bb93d2055dda4e5c5
cynthia <abahojomacynthia@gmail.com>
2025-11-28 16:11:56 +0100

    feat(resource-page): add new resource view page

--- components/contact-us/page.tsx
+++ components/contact-us/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-
-import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image'; // Import Image component
 import Input from '@/components/ui/input';
