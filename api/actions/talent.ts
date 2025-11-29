efdfe4b2108ca682ac326b18517a126aa57a0cd2
Sina-Olulana Hephzibah Oluwabusayo <93764058+Hephzibah-SO-Cu@users.noreply.github.com>
2025-11-29 21:44:37 +0100

    chore: fixed filters on Explore Jobs Page

--- api/actions/talent.ts
+++ api/actions/talent.ts
@@ -9,23 +9,9 @@ import { APIResponse, SuccessResponse } from '@/types/api-response';
 export const getTalentJobs = async (
   params?: TalentJobsQueryParams,
 ): Promise<APIResponse<RawJob2[]>> => {
-  const stringParams: Record<string, string> = {};
-  if (params) {
-    for (const [key, value] of Object.entries(params)) {
-      if (value !== undefined && value !== null) {
-        if (Array.isArray(value)) {
-          // Handle array values if necessary, e.g., by joining or sending multiple params
-          stringParams[key] = value.join(',');
-        } else {
-          stringParams[key] = String(value);
-        }
-      }
-    }
-  }
-
   return makeAuthenticatedRequest<RawJob2[]>('/talent/jobs', {
     method: 'GET',
-    params: stringParams,
+    params: params as Record<string, string>,
   });
 };
 
