ba1a6a4bf9e07fc18063001352e50efec9e63aca
Truella <turahsuleiman1212@gmail.com>
2025-12-05 08:41:28 +0100

    fix: refactor landing page header to have a dropdown

--- public/assets/images/landing-page/shared/constants.tsx
+++ public/assets/images/landing-page/shared/constants.tsx
@@ -1,3 +1,4 @@
+import { Icon } from 'lucide-react';
 import {
   FacebookIcon,
   HNGInternshipIcon,
@@ -10,14 +11,29 @@ import {
 } from './icons';
 
 export const NavLinks = [
-  { label: 'HNG Connect', href: '/', active: true, icon: <HNGPortalIcon /> },
   {
-    label: 'HNG Internship',
-    href: 'https://hng.tech/internship',
+    label: 'HNG Hire',
+    href: 'https://hng.tech/hire',
     icon: <HNGInternshipIcon />,
   },
-  { label: 'Learn', href: 'https://hng.tech/learn', icon: <LearnIcon /> },
+  {
+    label: 'Learn',
+    icon: <LearnIcon />,
+    dropdown: [
+      {
+        label: 'Learn',
+        href: 'https://hng.tech/learn',
+        icon: <HNGInternshipIcon />,
+      },
+      {
+        label: 'HNG Internship',
+        href: 'https://hng.tech/internship',
+        icon: <HNGInternshipIcon />,
+      },
+    ],
+  },
   { label: 'Premium', href: 'https://hng.tech/premium', icon: <PremiumIcon /> },
+  { label: 'HNG Connect', href: '/', active: true, icon: <HNGPortalIcon /> },
 ];
 
 export const LinkClasses =
